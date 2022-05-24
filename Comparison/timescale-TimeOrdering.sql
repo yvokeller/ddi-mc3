3dc8bc55c0a573c2304963b092dccdb2d4f8b406
Thomas Mandelz <tmandelz@hotmail.com>
2022-05-24 08:18:49 +0200

    add comparison sql files

---
+++ Comparison/timescale-TimeOrdering.sql
@@ -0,0 +1,8 @@
+-- ### Time Ordering ###
+-- @ TimescaleDB
+-- time ordering --> should make a big difference between base Postgres and TimescaleDB
+SELECT date_trunc('minute', time) AS minute, max(temperature)
+FROM api_measurement
+WHERE time > NOW() - INTERVAL '2 months' AND fk_measurement_station_id = 1
+GROUP BY minute
+ORDER BY minute ASC;
